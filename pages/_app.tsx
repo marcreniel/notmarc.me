1d4b08c22ac24b1cada3166557daf0ef4a13e79c
imedra <marcbernardino2005@gmail.com>
2022-12-23 22:05:43 -0600

    refined landing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
 import { useEffect } from "react";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     AOS.init({
       once: true,
       duration: 350,
-      easing: 'ease-out-cubic',
+      easing: "ease-out-cubic",
     });
   });
   return <Component {...pageProps} />
