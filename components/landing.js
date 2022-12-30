6551a7d393eaeae6daf4beb9a8e5c171d0e9e643
imedra <marcbernardino2005@gmail.com>
2022-12-30 02:46:48 -0600

    revamped landing (again); moved image assets to local

--- components/landing.js
+++ components/landing.js
@@ -1,30 +1,30 @@
 export default function Landing () {
     return (
-        <div id="home" className="bg-navy-blue text-white relative min-h-screen w-full items-center flex justify-center">
-            <div className="font-mono">
-                <div className="flex flex-col sm:flex-row">
-                    <img src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/320/microsoft/319/waving-hand_1f44b.png" className="object-contain h-32 max-w-sm rounded-lg pr-32 pb-5 sm:mr-0 sm:pb-0 sm:h-20 md:h-24 sm:pr-5"></img>
-                    <div className="flex flex-col">
-                        <h2 className="text-l sm:text-2xl md:text-3xl ">Hi! My name is</h2>
-                        <h1 className="typewriter reveal text-2xl font-bold sm:text-5xl md:text-6xl">Marc Bernardino</h1>  
-                        <br/>
-                        <nav className="flex z-10 flex-col md:text-xl">
-                            <button className="transition ease-in-out delay-150 hover:scale-110 duration-300 text-left">
-                            About
-                            </button>
-                            <button className="transition ease-in-out delay-150 hover:scale-110 duration-300 text-left">
-                            Blog
-                            </button>
-                            <button className="transition ease-in-out delay-150 hover:scale-110 duration-300 text-left">
-                            Projects
-                            </button>
-                            <button className="transition ease-in-out delay-150 hover:scale-110 duration-300 text-left">
-                            Contact
-                            </button>
-                        </nav>    
-                    </div>      
-                </div>    
-            </div>
+        <div id="home" className="bg-navy-blue text-white relative min-h-screen w-full items-center flex flex-col justify-center">
+            <div className="pb-36 sm:mt-28 font-mono flex flex-col sm:flex-row">
+                <img src="/hand.png" className="object-contain h-32 max-w-sm rounded-lg pr-32 pb-5 sm:mr-0 sm:pb-0 sm:h-20 sm:pr-5"></img>
+                <div className="flex flex-col">
+                    <h2 className="text-l sm:text-xl">Hi! My <span className="font-bold">name</span> is</h2>
+                    <h1 className="typewriter reveal text-2xl font-bold sm:text-5xl">Marc Bernardino</h1>
+                    <nav className="pt-2 flex z-10 flex-row space-x-5">
+                        <a href="https://linkedin.com/in/marcreniel/" target="_blank" rel="noopener" className="transition ease-in-out delay-150 hover:scale-110 duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="#f3f4f5" d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
+                        </a>
+                        <a href="https://github.com/imedra/" target="_blank" rel="noopener" className="transition ease-in-out delay-150 hover:scale-110 duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="#f3f4f5" d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/></svg>
+                        </a>
+                        <a href="https://twitter.com/notMarc_/" target="_blank" rel="noopener" className="transition ease-in-out delay-150 hover:scale-110 duration-300">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path fill="#f3f4f5" d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>                       
+                        </a>
+                    </nav> 
+                </div>
+            </div>   
+            <svg xmlns="http://www.w3.org/2000/svg" className="sm:mt-8 h-8 w-8" viewBox="0 0 32 32" aria-hidden="true">
+            <path fill="#f3f4f5" d="M16.003 18.626l7.081-7.081L25 13.46l-8.997 8.998-9.003-9 1.917-1.916z"/>
+            </svg> 
         </div>
       )
     }
