c3fa386581bc5693598a572c545fec65172e47f3
marcreniel <marcbernardino2005@gmail.com>
2024-10-04 02:30:54 -0700

    changed that

--- components/contact.jsx
+++ components/contact.jsx
@@ -21,7 +21,7 @@ export default function Contact (props) {
           </div>
           <div>
             <span className="footer-title">Resume</span> 
-            <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="link link-hover">Click here</a> 
+            <a href="/" target="_blank" rel="noopener noreferrer" className="link link-hover">Coming Soon</a> 
           </div>
       </div>
       </div>    
